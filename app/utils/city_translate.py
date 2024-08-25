521bda16d9aa9e5d1e6b15984acf59c4f6512a10
00-00-00\yustu <yustus.misha10@gmail.com>
2024-08-25 13:21:08 +0300

    no message

--- app/utils/city_translate.py
+++ app/utils/city_translate.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 
 def city_refractor(russian_city: str):
-    cities_data = pd.read_csv('C:/Users/yustu/ParserBack/ParserBack/app/database/data/cities.csv', header=None, names=['Russian', 'English'])
+    cities_data = pd.read_csv('./app/database/data/cities.csv', header=None, names=['Russian', 'English'])
     city_translation_map = dict(zip(cities_data['Russian'], cities_data['English']))
     selected_city_english = city_translation_map[russian_city].replace(" ", "_").lower()
     return selected_city_english
