18f83a9cab9301e39685c7a423bf265643982064
Justus Misha <90780967+justusmisha@users.noreply.github.com>
2024-11-19 14:39:45 +0300

    Update city_translate.py

--- app/utils/city_translate.py
+++ app/utils/city_translate.py
@@ -6,5 +6,6 @@ import pandas as pd
 def city_refractor(russian_city: str):
     cities_data = pd.read_csv('./app/database/data/cities.csv', header=None, names=['Russian', 'English'])
     city_translation_map = dict(zip(cities_data['Russian'], cities_data['English']))
+    print(city_translation_map)
     selected_city_english = city_translation_map[russian_city].replace(" ", "_").capitalize()
     return selected_city_english
